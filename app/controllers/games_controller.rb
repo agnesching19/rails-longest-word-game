7edb0488a9e987081567b411ad24992390a31e96
Agnes Ching <agnesching19@gmail.com>
2017-11-06 22:19:18 +0000

    modified version

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,32 +1,30 @@
 require 'open-uri'
-require 'json'
 
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @letters = Array.new(10) { ("A".."Z").to_a.sample }
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
   end
 
   def english_word?(word)
     response = open("https://wagon-dictionary.herokuapp.com/#{word}")
     json = JSON.parse(response.read)
-    return json['found']
-  end
-
-  def word_in_grid?(word, grid)
-    word.upcase.split("").all? {|char| word.count(char) <= grid.count(char) }
-  end
-
-  def score
-    grid = params[:grid].split("")
-    word = params[:answer]
-    @score = {}
-    if english_word?(word) && word_in_grid?(word, grid)
-      @score[:message] = "Well done"
-      @score[:result] = 5
-    elsif english_word?(word)
-      @score[:message] = "Sorry it's out of the grid"
-    else
-      @score[:message] = "Sorry it's not a valid English word"
-    end
+    json['found']
   end
 end
