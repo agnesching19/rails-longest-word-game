c1bb45c8a67cc73ec2dee5943397578ccc5fbe57
Agnes Ching <agnesching19@gmail.com>
2018-01-15 17:24:28 +0000

    changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'new', to: 'games#new'
 
+  root to: "games#new"
   post 'score', to: 'games#score'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
